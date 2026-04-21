a25bd2fee1f12aceab415ffacab26d2a99682f12
casperrobieniek6-ops <casperrobieniek6@gmail.com>
2026-04-21 17:23:19 -0400

    Main + CSreader_service

--- services/CSVreader_service.py
+++ services/CSVreader_service.py
@@ -1,1 +1,12 @@
 import csv
+
+#Class for CSV Reader
+class CSVreader_service:
+    def __init__(self):
+        pass
+    #Method for opening file and converting to dic
+    def read_file(self, file_name):
+        with open(file_name) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+            
